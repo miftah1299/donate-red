010f6d55e11c39925859ce77b049935e1f23e498
miftah1299 <miftahjannat25@gmail.com>
2025-01-16 01:58:43 +0600

    added site settings

--- donations.php
+++ donations.php
@@ -1,69 +1,116 @@
 <?php include('db_connect.php'); ?>
 
-<div class="container mx-auto p-4">
-    <div class="flex justify-end mb-4">
-        <button class="btn btn-primary" id="new_donation">
-            <i class="fa fa-plus"></i> New Entry
-        </button>
-    </div>
-    <div class="card w-full bg-white shadow-md">
-        <div class="card-body">
-            <h2 class="card-title text-2xl font-bold mb-4">List of Donations</h2>
-            <div class="overflow-x-auto">
-                <table class="table w-full">
-                    <thead>
-                        <tr>
-                            <th class="text-center">#</th>
-                            <th>Date</th>
-                            <th>Donor</th>
-                            <th>Blood Group</th>
-                            <th>Volume (ml)</th>
-                            <th class="text-center">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        $i = 1;
-                        $donations = $conn->query("SELECT d.*, concat(d.lastname, ', ', d.firstname, ' ', d.middlename) as name, b.blood_group FROM donations d inner join donors b on b.id = d.donor_id order by unix_timestamp(d.date_created) desc ");
-                        while ($row = $donations->fetch_assoc()):
-                        ?>
-                            <tr>
-                                <td class="text-center"><?php echo $i++ ?></td>
-                                <td><?php echo date("M d, Y", strtotime($row['date_created'])) ?></td>
-                                <td><?php echo ucwords($row['name']) ?></td>
-                                <td><?php echo $row['blood_group'] ?></td>
-                                <td><?php echo $row['volume'] ?></td>
-                                <td class="text-center">
-                                    <button class="btn btn-sm btn-primary edit_donation" data-id="<?php echo $row['id'] ?>">Edit</button>
-                                    <button class="btn btn-sm btn-error delete_donation" data-id="<?php echo $row['id'] ?>">Delete</button>
-                                </td>
-                            </tr>
-                        <?php endwhile; ?>
-                    </tbody>
-                </table>
+<div class="container-fluid">
+
+    <div class="col-lg-12">
+        <div class="row mb-4 mt-4">
+            <div class="col-md-12">
+
             </div>
         </div>
-    </div>
-</div>
+        <div class="row">
+            <!-- FORM Panel -->
 
+            <!-- Table Panel -->
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <b>List of Donations</b>
+                        <span class="float:right"><a class="btn btn-primary btn-block btn-sm col-sm-2 float-right" href="javascript:void(0)" id="new_donation">
+                                <i class="fa fa-plus"></i> New Entry
+                            </a></span>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-condensed table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th class="text-center">#</th>
+                                    <th class="">Date</th>
+                                    <th class="">Donor</th>
+                                    <th class="">Blood Group</th>
+                                    <th class="">Volume (ml)</th>
+                                    <th class="text-center">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $i = 1;
+                                $donor = $conn->query("SELECT * FROM donors");
+                                while ($row = $donor->fetch_assoc()) {
+                                    $dname[$row['id']] = ucwords($row['name']);
+                                }
+                                $donations = $conn->query("SELECT * FROM blood_inventory where status = 1 order by date(date_created) desc ");
+                                while ($row = $donations->fetch_assoc()):
+
+                                ?>
+                                    <tr>
+                                        <td class="text-center"><?php echo $i++ ?></td>
+                                        <td>
+                                            <?php echo date('M d, Y', strtotime($row['date_created'])) ?>
+                                        </td>
+                                        <td class="">
+                                            <p> <b><?php echo isset($dname[$row['donor_id']]) ? $dname[$row['donor_id']] : 'Donor was removed from the list.' ?></b></p>
+                                        </td>
+                                        <td class="">
+                                            <p> <b><?php echo $row['blood_group'] ?></b></p>
+                                        </td>
+                                        <td class="">
+                                            <p><b><?php echo $row['volume']; ?></b></p>
+                                        </td>
+                                        <td class="text-center">
+                                            <button class="btn btn-sm btn-outline-primary edit_donation" type="button" data-id="<?php echo $row['id'] ?>">Edit</button>
+                                            <button class="btn btn-sm btn-outline-danger delete_donation" type="button" data-id="<?php echo $row['id'] ?>">Delete</button>
+                                        </td>
+                                    </tr>
+                                <?php endwhile; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <!-- Table Panel -->
+        </div>
+    </div>
+
+</div>
+<style>
+    td {
+        vertical-align: middle !important;
+    }
+
+    td p {
+        margin: unset
+    }
+
+    img {
+        max-width: 100px;
+        max-height: 150px;
+    }
+</style>
 <script>
+    $(document).ready(function() {
+        $('table').dataTable()
+    })
+
     $('#new_donation').click(function() {
-        uni_modal('New Donation', 'manage_donation.php')
+        uni_modal("New donation", "manage_donation.php", "mid-large")
+
     })
     $('.edit_donation').click(function() {
-        uni_modal('Edit Donation', 'manage_donation.php?id=' + $(this).attr('data-id'))
+        uni_modal("Manage donation Details", "manage_donation.php?id=" + $(this).attr('data-id'), "mid-large")
+
     })
     $('.delete_donation').click(function() {
         _conf("Are you sure to delete this donation?", "delete_donation", [$(this).attr('data-id')])
     })
 
-    function delete_donation(id) {
+    function delete_donation($id) {
         start_load()
         $.ajax({
             url: 'ajax.php?action=delete_donation',
             method: 'POST',
             data: {
-                id: id
+                id: $id
             },
             success: function(resp) {
                 if (resp == 1) {
@@ -71,6 +118,7 @@
                     setTimeout(function() {
                         location.reload()
                     }, 1500)
+
                 }
             }
         })
